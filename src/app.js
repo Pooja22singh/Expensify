252de95094ec3e11785b877a444a4ed52c1d805f
Pooja Singh <singhpooja0493@gmail.com>
2020-04-09 03:03:38 +0530

    Hard Coded Expenses Removed

--- src/app.js
+++ src/app.js
@@ -8,35 +8,6 @@ import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
 
 const store = configureStore();
-//Add Expenses
-
-const expenseOne = store.dispatch(addExpense(
-    { description : "Water Bill",
-      note : "water bill payment",
-      amount : 1000, 
-      createDate : 12345
-    }));
-
-const expenseTwo=store.dispatch(addExpense(
-    { description : "Electricity Bill",
-      note : "electricity bill payment",
-      amount : 500, 
-      createDate : 12346
-    }));
-
-const expenseThree=store.dispatch(addExpense(
-    { description : "Maintenance charges",
-      note : "Miscelleneous payment",
-      amount : 800, 
-      createDate : 12347
-    }));
-
-//Setting Text Filter
-
-//store.dispatch(setFilterText("bill"));
-// setTimeout(()=>{
-//   store.dispatch(setFilterText("rent"));
-// },3000);
 
 const jsx=(
   <Provider store = {store}>
