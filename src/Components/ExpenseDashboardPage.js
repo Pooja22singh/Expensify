de2073e23ccd643696a971edf737d99719b7a7ad
Pooja Singh <singhpooja0493@gmail.com>
2020-04-10 02:02:18 +0530

    Expense Summary Component

--- src/Components/ExpenseDashboardPage.js
+++ src/Components/ExpenseDashboardPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpensesListFilter from './ExpensesListFilter';
+import ExpensesSummary from './ExpensesSummary';
 const ExpenseDashboardPage = () => (
     <div>
+    <ExpensesSummary/>
     <ExpensesListFilter/>
     <ExpenseList/>
     </div>
--- src/Components/ExpenseListItem.js
+++ src/Components/ExpenseListItem.js
@@ -4,6 +4,6 @@ import moment from 'moment';
 export default ( {id, description, amount, createDate} ) => (
     <div>
         <h3><Link to={`/edit/${ id }`}>{description}</Link></h3>
-        <p>Amount : { amount }, Created At : {moment(createDate).format("DD MMM YYYY")}</p>
+        <p>Amount : {amount}, Created At : {moment(createDate).format("DD MMM YYYY")}</p>
     </div>
-);
+)
